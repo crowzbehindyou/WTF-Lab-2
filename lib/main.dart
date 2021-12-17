6da330d768605ba07e249059f5eb03dd23e3da37
crowzbehindyou <91633117+crowzbehindyou@users.noreply.github.com>
2021-12-17 17:36:51 +0500

    'new_functional'

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/messages': (context) => const MessageScreen(),
+      },
       theme: ThemeData(
         primaryColor: Colors.deepOrangeAccent
       ),
       title: 'Startup Name Generator',
-      home: Scaffold(
+      );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController = PageController(
+  );
+  int _selectedIndex = 0;
+  void onTapped (int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.jumpToPage(index);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepOrangeAccent,
           title: const Text('Пробуем'),
@@ -20,23 +49,23 @@ class MyApp extends StatelessWidget {
             onPressed: () {},
           ),
         ),
-        body: const ListOfChats(),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepOrangeAccent,
-          child: const Icon(Icons.add),
-          onPressed: () {},
+        body: PageView(
+          controller: pageController,
+          children: [const ListOfChats(),
+          const Events()],
         ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home)),
-          const BottomNavigationBarItem(
-              label: 'Delete',
-              icon: Icon(Icons.delete))
-        ],
-      ),
-      )
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTapped,
+          currentIndex: _selectedIndex,
+          items: [
+            const BottomNavigationBarItem(
+                label: 'Сообщения',
+                icon: Icon(Icons.home)),
+            const BottomNavigationBarItem(
+                label: 'Список дел',
+                icon: Icon(Icons.event))
+          ],
+        )
     );
   }
 }
@@ -47,21 +76,105 @@ class ListOfChats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 10,
+        itemCount: 2,
         itemBuilder: (context, index) {
       return GestureDetector(
-        onTap: () {print('$index');},
+        onTap: () {
+          Navigator.of(context).pushNamed('/messages');
+        },
           child: Container(
-            color: index % 2 == 0 ? Colors.green : Colors.red,
+            color: Colors.grey,
             child: Row(
               children: [
                 const CircleAvatar(
+                  backgroundImage: AssetImage('assets/avatars/sizif.jpg')),
+                const Text('Друг')
 
-                )
               ],
             ),
       )
       );
     });
+  }
+}
+
+
+class Events extends StatefulWidget {
+  const Events({Key? key}) : super(key: key);
+
+  @override
+  _EventsState createState() => _EventsState();
+}
+
+class _EventsState extends State<Events> {
+  late String _userToDo;
+  List<String> todoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Dismissible(key: Key(todoList[index]),
+          onDismissed: (direction) {
+            setState(() {
+          todoList.removeAt(index);
+        });
+          },
+          child: Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          )
+      );
+    }
+    ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrangeAccent,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showDialog(context: context, builder: (context) {
+            return AlertDialog(
+              title: const Text('Добавить событие'),
+              content: TextField(
+                onChanged: (value) {
+                  _userToDo = value;
+                },
+              ),
+              actions: [
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    todoList.add(_userToDo);
+                  });
+                  Navigator.of(context).pop();
+                }, child: const Text('ADD'))
+              ],
+            );
+          });
+        },
+      ),
+    );
+  }
+}
+
+class MessageScreen extends StatefulWidget {
+  const MessageScreen({Key? key}) : super(key: key);
+
+  @override
+  _MessageScreenState createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends State<MessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
